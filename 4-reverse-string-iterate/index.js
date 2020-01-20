9367a0112d00d2bece9ea3b5146b74dcfbf8849e
cuong198314 <cuong198314@gmail.com>
2020-01-19 23:03:32 -0800

    complete string iterate

--- 4-reverse-string-iterate/index.js
+++ 4-reverse-string-iterate/index.js
@@ -1,9 +1,9 @@
 //TODO fill in comment template
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that takes a string as input and reverses it by iterating through the string, character by character.
+Input: string
+Output: reverse string character by character
+Usage: reverse string iterate
 */
 
 // get input using getargs
@@ -13,26 +13,29 @@ let input = getargs.getStringArg()
 function reverseWithFullIteration(str) {
     let result = '';
     // TODO loop from end of str (str.length-1) to beginning
-    for ( ) {
+    for (let i = str.length-1; i>=0; i--) {
+        result += str[i];
+        
         // TODO add the letter in the current position to the result string.
     }
     return result;
 }
 
 function reverseWithHalfIteration(str) {
-    // STRETCH TODO write a function that reverses the string but only iterates over half of the input string.
+    
+    
 }
 
 //test that it is a string
 //TODO check for string argument and if no string is entered provide a usage statement and quit
-if ( ) {
-    // TODO provide a usage statement
-}
+if (!input ) {
+    console.log("usage: node 4-reverse-string-iterate <string>");
+ }
 else {
     // use functions to reverse string
     console.log("result with full iteration", reverseWithFullIteration(input));
 
-    // STRETCH TODO output the result of your reverseWithHalfIteration function
+   
 
 }
 
